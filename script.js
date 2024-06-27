d6855616af12f8396d5a7e73a88cd3217b7955d8
Vidhi Agarwal <165956295+vidhigrwl@users.noreply.github.com>
2024-06-28 02:05:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 window.addEventListener('scroll', function() {
     var header = document.getElementById('header');
     if (window.scrollY > 0) {
-      header.style.backgroundColor = '#555'; // Change background color when scrolled
+      header.style.backgroundColor = '#575151'; // Change background color when scrolled
     } else {
-      header.style.backgroundColor = '#333'; // Revert to original color when at the top
+      header.style.backgroundColor = '#090808'; // Revert to original color when at the top
     }
   });
-
